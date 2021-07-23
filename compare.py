1c54c987235ea0202bdb48a523285a332b60b90a
Jonathan Licari <jonathan.licari@aviyatech.com>
2021-07-23 12:12:09 -0400

    Create hash table for each baseline version

--- compare.py
+++ compare.py
@@ -1,35 +1,50 @@
 import pandas as pd
 from pathlib import Path
 
-# Import 
-source_folder = Path("Z:/WWD_Serv/Team/JonathanL/")
+# Path assignment  
+source_folder = Path("Z:/WWD_Serv/Team/JonathanL/CIA/")
+output_folder = Path("Z:/WWD_Serv/Team/JonathanL/CIA/output")
+
+# Baseline Column Headers  
 baseline_ID_title = "ID"
 baseline_value_title = "Last Modified On"
 
+# Current Revision Column Headers  
 current_ID_title = "ID"
 current_value_title = "Last Modified On"
 
-# import baseline excel file
+# -------------------------- #
+# Import baseline excel file #
+# -------------------------- #
+
 baseline = pd.read_excel(r"RD_14.1_Export.xlsx", sheet_name='Sheet1')
-baseline_keys = pd.DataFrame(baseline, columns = [baseline_ID_title])
-baseline_values = pd.DataFrame(baseline, columns = [baseline_value_title])
+# Remove unwanted data
+baseline = baseline.drop(
+    ['Object Heading', 'WWD_Derived', 'New CM Tag', 'CM Tag', 'EPECS Requirements Data Dictionary'],
+ axis=1)
 
-# import current version excel file
+# Create baseline hash map
+baseline_hash = {}
+basline_keys = baseline[baseline_ID_title].to_list()
+baseline_values = baseline[baseline_value_title]
+baseline_hash = dict(zip(basline_keys, baseline_values))
+#print(baseline_hash['RD60519'])
+
+# --------------------------------- #
+# import current version excel file #
+# --------------------------------- #
+
 current = pd.read_excel(r"RD_10.21_Export.xlsx", sheet_name= 'Sheet1')
-current_keys = pd.DataFrame(current, columns = [current_ID_title])
-current_values = pd.DataFrame(current, columns = [current_value_title])
+# Remove unwanted data
+current = current.drop(
+    ['Object Heading', 'WWD_Derived', 'New CM Tag', 'CM Tag', 'EPECS Requirements Data Dictionary'],
+ axis=1)
 
-# Check types
-print("Current Revision Hash key type: ")
-print(type(current_keys))
-print("Current Revision Hash value type: ")
-print(type(current_values))
+# Create current revision hash map 
+current_hash = {}
+current_keys = current[current_ID_title].to_list()
+current_values = current[current_value_title]
+current_hash = dict(zip(current_keys, current_values))
+#print(current_hash['RD60519'])
 
-print("Baseline Revision Hash key type: ")
-print(type(baseline_keys))
-print("Current Revision Hash value type: ")
-print(type(baseline_values))
-
-print(baseline)
-print(baseline_keys)
-
+# Export results in spreadsheet
